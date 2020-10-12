c25afef72b36fbc6da8fd040b7023be3b98bee19
lucy <frenchline707@gmail.com>
2020-10-13 02:38:41 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/Hello.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by frenchline707@gmail.com on 2020-10-13
+ * Blog : http://frenchline707.tistory.com
+ * Github : http://github.com/frenchLineCigar
+ */
+
+@Getter @Setter
+public class Hello {
+    private String data;
+}
