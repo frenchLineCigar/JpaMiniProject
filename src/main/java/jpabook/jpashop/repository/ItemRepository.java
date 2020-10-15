8442f988ad11071f56b7679b565dfb8444045150
lucy <frenchline707@gmail.com>
2020-10-16 08:35:50 +0900

    상품 레파지토리 개발
    - 상품 등록
    - 상품 조회(단건)
    - 상품 전체 조회 (상품 목록)
    - 이름으로 상품 조회(추가)
    
    -----------
    섹션 5. 상품 도메인 개발
    
    # 구현 기능
    상품 등록
    상품 목록 조회
    상품 수정
    
    # 순서
    상품 엔티티 개발(비즈니스 로직 추가)
    상품 리포지토리 개발
    상품 서비스 개발
    상품 기능 테스트

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+/**
+ * 상품 레파지토리
+ */
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    /**
+     * 상품 등록
+     */
+    public void save(Item item) {
+        if (item.getId() == null) { //아이디가 없으면 새걸로 보고 persist() 호출
+            em.persist(item); //신규 등록 - Make an instance managed and persistent.
+        } else { //아이디가 있으면 merge() 호출
+            em.merge(item); //Merge the state of the given entity into the current persistence context.
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class,id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+
+    //추가 해본 것
+    public List<Item> findByName(String name) {
+        return em.createQuery("select i from Item i where i.name =: name",Item.class)
+                .getResultList();
+    }
+
+}
