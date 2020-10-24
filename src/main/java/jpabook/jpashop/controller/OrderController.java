93db49be7460ac05120a2d18144b5ae6617286cc
lucy <frenchline707@gmail.com>
2020-10-24 21:40:58 +0900

    웹 계층 개발 > 상품 주문
    * 상품 주문과 관련된 메서드 추가
    OrderController.createForm
    OrderController.order
    
    * 상품 주문 폼 화면 추가
    orderForm.html

---
+++ src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -0,0 +1,51 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.service.ItemService;
+import jpabook.jpashop.service.MemberService;
+import jpabook.jpashop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+/**
+ * 주문 컨트롤러
+ */
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/order")
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping
+    public String createForm(Model model) {
+
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+
+        orderService.order(memberId, itemId, count); //여러개 상품을 주문할 수 있도록 로직 변경해보자.
+        return "redirect:/orders";
+    }
+
+}
