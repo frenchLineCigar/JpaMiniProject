3e10ffeb51dc10929a080ba693dcf77eb6b7630d
lucy <frenchline707@gmail.com>
2020-10-28 16:54:50 +0900

    API 개발 고급 > 컬렉션 조회 최적화 > 주문 조회 V2: 엔티티를 DTO로 변환
    * 지연 로딩으로 너무 많은 SQL 실행
    * SQL 실행 수
        * order 1번
        * member , address N번(order 조회 수 만큼)
        * orderItem N번(order 조회 수 만큼)
        * item N번(orderItem 조회 수 만큼)
    
    > 참고: 지연 로딩은 영속성 컨텍스트에 있으면 영속성 컨텍스트에 있는 엔티티를 사용하고 없으면 SQL을 실
    행한다. 따라서 만약 같은 영속성 컨텍스트에서 이미 로딩한 회원 엔티티를 추가로 조회하면 SQL을 실행하지 않는다.

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,22 +1,28 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 주문 ( + 배송 + 회원 + 주문 상품 ) 정보 조회 API
  *
  * V1. 엔티티 직접 노출
- * - 엔티티가 변하면 API 스펙이 변한다.
- * - 트랜잭션 안에서 지연 로딩 필요
- * - 양방향 연관관계 문제
+ * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
+ *
  */
 @RestController
 @RequiredArgsConstructor
@@ -35,9 +41,65 @@ public class OrderApiController {
             order.getMember().getName(); //Lazy 강제 초기화, Member
             order.getDelivery().getAddress(); //Lazy 강제 초기화, Delivery
 
-            List<OrderItem> orderItems = order.getOrderItems(); //Lazy 강제 초기화, OrderItem
+            List<OrderItem> orderItems = order.getOrderItems();
             orderItems.stream().forEach(o -> o.getItem().getName()); //Lazy 강제 초기화, Item
         }
         return all;
     }
+
+    /**
+     * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
+     * - 트랜잭션 안에서 지연 로딩 필요
+     */
+    @GetMapping("/api/v2/orders")
+    public Result ordersV2() {
+        List<Order> orders = orderRepository.findAll(new OrderSearch());
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return new Result(result);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Getter
+    static class OrderDto {
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Getter
+    static class OrderItemDto {
+
+        private String itemName; //상품 명
+        private int orderPrice; //주문 가격
+        private int count; //주문 수량
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
+    }
+
 }
